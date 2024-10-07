d2e50b765799f07eac404bf835d070d0f4c59871
tsubasa2911 <randoctor11@gmail.com>
2024-10-07 20:13:12 +0900

    ファイル追加

---
+++ kadai_mdn_js/bmi.js
@@ -0,0 +1,7 @@
+let weight = 68;
+
+let hight = 1.7;
+
+let bmi = weight / (hight * hight);
+
+console.log(bmi);
---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+
+const today = new Date('2024-10-09');
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+console.log(year + '年' + month + '月' + date +'日');
